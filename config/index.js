60f426c5e84fc4b078b4a70397b4e73acaa2a5f9
lidonghai <627247084@qq.com>
2021-04-14 16:35:13 +0800

    修改打包资源路径

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: "static",
-    assetsPublicPath: "./",
+    assetsPublicPath: "/",
 
     // Various Dev Server settings
     host: "localhost", // can be overwritten by process.env.HOST
@@ -46,8 +46,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, "../dist"),
-    assetsSubDirectory: "static",
-    assetsPublicPath: "/",
+    assetsSubDirectory: "./static",
+    assetsPublicPath: "./",
 
     /**
      * Source Maps
